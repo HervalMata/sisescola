542e99bb0f700d086678a56819dd7376ad398f23
HervalMata <hervalmataamparo@gmail.com>
2018-12-21 14:38:19 -0300

    Trabalhando com datas nos formulários

--- app/Forms/ClassInformationForm.php
+++ app/Forms/ClassInformationForm.php
@@ -2,20 +2,26 @@
 
 namespace SON\Forms;
 
+use Carbon\Carbon;
 use Kris\LaravelFormBuilder\Form;
 
 class ClassInformationForm extends Form
 {
     public function buildForm()
     {
+        $formatDate = function ($value) {
+            return ($value && $value instanceof Carbon) ? $value->format('Y-m-d'): $value;
+        };
         $this
             ->add('date_start', 'date', [
                 'label' => 'Data Inicio',
-                'ruled' => 'required|date'
+                'ruled' => 'required|date',
+                'value' => $formatDate
             ])
             ->add('date_end', 'date', [
                 'label' => 'Data Final',
-                'ruled' => 'required|date'
+                'ruled' => 'required|date',
+                'value' => $formatDate
             ])
             ->add('cycle', 'number', [
                 'label' => 'Ciclo',
