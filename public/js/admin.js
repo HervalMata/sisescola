03cced93424a98d7aae37f3c8f51a9abf751fd99
HervalMata <hervalmataamparo@gmail.com>
2018-12-22 03:27:08 -0300

    Buscando alunos no select

--- public/js/admin.js
+++ public/js/admin.js
@@ -24793,7 +24793,7 @@ exports = module.exports = __webpack_require__(17)(false);
 
 
 // module
-exports.push([module.i, "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n", ""]);
+exports.push([module.i, "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n", ""]);
 
 // exports
 
@@ -25194,8 +25194,20 @@ Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
                 url: __WEBPACK_IMPORTED_MODULE_0__services_adminConfig__["a" /* default */].API_URL + '/students',
                 dataType: 'json',
                 delay: 250,
-                processResults: function processResults(data) {}
-            }
+                data: function data(params) {
+                    return {
+                        q: params.term
+                    };
+                },
+                processResults: function processResults(data) {
+                    return {
+                        results: data.map(function (student) {
+                            return { id: student.id, text: student.user.name };
+                        })
+                    };
+                }
+            },
+            minimumInputLength: 1
         });
     }
 });
