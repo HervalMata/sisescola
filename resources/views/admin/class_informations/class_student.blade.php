6f8cec68eab435bf945b1e88a1f01d933b19f4be
HervalMata <hervalmataamparo@gmail.com>
2018-12-22 01:15:57 -0300

    Configuração inicial com Laravel Mix

--- resources/views/admin/class_informations/class_student.blade.php
+++ resources/views/admin/class_informations/class_student.blade.php
@@ -5,6 +5,7 @@
         <div class="row">
             <h3>Administração de alunos na turma</h3>
             <!-- Criar componente Vue.js -->
+            <class-student></class-student>
         </div>
     </div>
 @endsection
